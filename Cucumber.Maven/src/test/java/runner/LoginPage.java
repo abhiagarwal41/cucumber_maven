2644a7dd62b14e1fc89f5fd0315478bc21b3e9a8
abhiagarwal41 <abhiagarwal41@gmail.com>
2018-05-23 10:46:25 +0530

    Update LoginPage.java

--- Cucumber.Maven/src/test/java/runner/LoginPage.java
+++ Cucumber.Maven/src/test/java/runner/LoginPage.java
@@ -13,60 +13,58 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.runner.SingletonClass;
 
-public class LoginPage extends AbstractPage{
-    
+public class LoginPage extends AbstractPage {
+
 	
-	public LoginPage(WebDriver driver) {
-		super(driver);
-	}
-
-	@FindBy(id="email") 
+	@FindBy(id = "email")
 	@CacheLookup
-	public static WebElement email; 
+	public static WebElement email;
 
-	@FindBy(id="passwd")
+	@FindBy(id = "passwd")
 	@CacheLookup
 	public static WebElement password;
 
-	@FindBy(how=How.XPATH,using=".//*[@id='SubmitLogin']")
+	@FindBy(how = How.XPATH, using = ".//*[@id='SubmitLogin']")
 	@CacheLookup
 	public static WebElement submit_button;
-	
-	@FindBy(how=How.XPATH,using=".//*[@id='center_column']/div[1]/ol/li")
+
+	@FindBy(how = How.XPATH, using = ".//*[@id='center_column']/div[1]/ol/li")
 	@CacheLookup
 	public static WebElement error_message;
-	
-	public WebElement getWebElement(String name){
-		/*String cssselector="";
-		switch(name.toUpperCase()){
-		case "EMAIL" : cssselector= "#email"; break;
-		case "PASSWORD" : cssselector= "#passwd"; break;
-		case "SUBMIT BUTTON" : cssselector= "#SubmitLogin"; break;
-		case "ERROR MESSAGE" : cssselector= "#center_column div.alert ol li"; break;
-		default:
-			throw new NoSuchElementException(name);
-		}
-		return SingletonClass.getInstance().getDriver().findElement(By.cssSelector(cssselector));*/
-		
-		switch(name.toUpperCase()){
-		case "EMAIL" : return email;
-		case "PASSWORD" : return password;
-		case "SUBMIT BUTTON" : return submit_button;
-		case "ERROR MESSAGE" : return error_message;
+
+	public WebElement getWebElement(String name) {
+		/*
+		 * String cssselector=""; switch(name.toUpperCase()){ case "EMAIL" :
+		 * cssselector= "#email"; break; case "PASSWORD" : cssselector=
+		 * "#passwd"; break; case "SUBMIT BUTTON" : cssselector= "#SubmitLogin";
+		 * break; case "ERROR MESSAGE" : cssselector=
+		 * "#center_column div.alert ol li"; break; default: throw new
+		 * NoSuchElementException(name); } return
+		 * SingletonClass.getInstance().getDriver().findElement(By.cssSelector(
+		 * cssselector));
+		 */
+
+		switch (name.toUpperCase()) {
+		case "EMAIL": 
+			return email;
+		case "PASSWORD":
+			return password;
+		case "SUBMIT BUTTON":
+			return submit_button;
+		case "ERROR MESSAGE":
+			return error_message;
 		default:
 			throw new NoSuchElementException(name);
 		}
 	}
 
-
-	public void login_wordpress(String uid,String pass)
-	{
+	public void login_wordpress(String uid, String pass) {
 		email.sendKeys(uid);
-	password.sendKeys(pass);
-	submit_button.click();
+		password.sendKeys(pass);
+		submit_button.click();
 	}
-	
-	public String getErrorMessage(){
+
+	public String getErrorMessage() {
 		return error_message.getText();
 	}
 
