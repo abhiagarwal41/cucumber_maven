60f366ca9fd5d5054ec2f3cb3eef4867cfe3f3df
abhiagarwal41 <abhiagarwal41@gmail.com>
2018-05-23 10:45:40 +0530

    Update AbstractPage.java

--- Cucumber.Maven/src/test/java/runner/AbstractPage.java
+++ Cucumber.Maven/src/test/java/runner/AbstractPage.java
@@ -1,5 +1,7 @@
 package com.pages;
 
+import static com.runner.SingletonClass.getInstance;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
@@ -9,12 +11,7 @@ import com.runner.SingletonClass;
 
 public abstract class AbstractPage {
 
-	protected static WebDriver driver;
 	private static AbstractPage testPage;
-
-	public AbstractPage(WebDriver driver) {
-		driver = driver;
-	}
 	
 	public static AbstractPage getTestPage(){
 		return testPage;
@@ -23,12 +20,12 @@ public abstract class AbstractPage {
 	public abstract WebElement getWebElement(String name);
 	
 	public static void setTestPage(String pageName){
-	    driver = SingletonClass.getInstance().getDriver();
+	  
 		switch(pageName.toLowerCase()){
-		case "loginpage" :  testPage = new LoginPage(driver);
-		PageFactory.initElements(SingletonClass.getInstance().getDriver(), LoginPage.class);break;
-		case "homepage" :  testPage = new HomePage(driver); 
-		PageFactory.initElements(SingletonClass.getInstance().getDriver(), HomePage.class);break;
+		case "loginpage" :  testPage = new LoginPage();
+		PageFactory.initElements(getInstance().getDriver(), LoginPage.class);break;
+		case "homepage" :  testPage = new HomePage(); 
+		PageFactory.initElements(getInstance().getDriver(), HomePage.class);break;
 		default:
 			System.out.println("No such page exists: " +pageName );
 		}
